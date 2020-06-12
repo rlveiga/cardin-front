34b5c00a957ff6b6139a941ae9943d22bfe87fa3
Rodrigo Veiga <rodrigoveiga1206@gmail.com>
2020-06-12 18:26:37 -0300

    Updating PlayersList with ScrollView

--- src/components/PlayersList.js
+++ src/components/PlayersList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
 import { heightPercentageToDP, widthPercentageToDP } from 'react-native-responsive-screen';
 import { inject, observer } from 'mobx-react';
 import PlayerPreview from './PlayerPreview';
@@ -11,58 +11,58 @@ export default class PlayersList extends Component {
   renderPlayers() {
     return this.props.room.currentRoom.game && this.props.room.playersList ?
       this.props.room.playersList
-      .slice()
-      .sort((a, b) => {
-        return b.score - a.score
-      })
-      .map((player, i) => {
-        const isVoter = this.props.room.currentRoom.game.czar_id == player.data.id
+        .slice()
+        .sort((a, b) => {
+          return b.score - a.score
+        })
+        .map((player, i) => {
+          const isVoter = this.props.room.currentRoom.game.czar_id == player.data.id
 
-        return (
-          <View
-            style={{
-              alignItems: 'center',
-              marginHorizontal: 6,
-            }}
-            key={i}>
-            {
-              player.data.source == 'fb' ?
-                <Image
-                  style={{
-                    height: heightPercentageToDP(8),
-                    width: heightPercentageToDP(8),
-                    borderRadius: heightPercentageToDP("4%"),
-                  }}
-                  source={{
-                    uri: player.data.profile_img
-                  }} /> :
-                <PlayerPreview
-                  fontSize={28}
-                  player={player.data}
-                  score={player.score} />
-            }
-
+          return (
             <View
               style={{
-                backgroundColor: isVoter ? '#000' : '#FFF',
-                borderRadius: heightPercentageToDP(4),
-                borderWidth: isVoter ? 2 : 0,
-                borderColor: '#FFF',
-                justifyContent: 'center',
-                marginTop: heightPercentageToDP(1),
-                paddingHorizontal: widthPercentageToDP(1)
-              }}>
-              <Text style={{ color: isVoter ? '#FFF' : '#000', textAlign: 'center' }}>
-                {
-                  player.data.source == 'fb' ?
-                    player.data.name.split(' ')[0] :
-                    player.data.username
-                }
-              </Text>
+                alignItems: 'center',
+                marginHorizontal: 6,
+              }}
+              key={i}>
+              {
+                player.data.source == 'fb' ?
+                  <Image
+                    style={{
+                      height: heightPercentageToDP(8),
+                      width: heightPercentageToDP(8),
+                      borderRadius: heightPercentageToDP("4%"),
+                    }}
+                    source={{
+                      uri: player.data.profile_img
+                    }} /> :
+                  <PlayerPreview
+                    fontSize={28}
+                    player={player.data}
+                    score={player.score} />
+              }
+
+              <View
+                style={{
+                  backgroundColor: isVoter ? '#000' : '#FFF',
+                  borderRadius: heightPercentageToDP(4),
+                  borderWidth: isVoter ? 2 : 0,
+                  borderColor: '#FFF',
+                  justifyContent: 'center',
+                  marginTop: heightPercentageToDP(1),
+                  paddingHorizontal: widthPercentageToDP(1)
+                }}>
+                <Text style={{ color: isVoter ? '#FFF' : '#000', textAlign: 'center' }}>
+                  {
+                    player.data.source == 'fb' ?
+                      player.data.name.split(' ')[0] :
+                      player.data.username
+                  }
+                </Text>
+              </View>
             </View>
-          </View>
-        )
-      }) :
+          )
+        }) :
       this.props.room.currentRoom.users.map((player, i) => {
         return (
           <View
@@ -115,7 +115,10 @@ export default class PlayersList extends Component {
     return (
       <View
         style={styles.container}>
-        {this.renderPlayers()}
+        <ScrollView
+        horizontal={true}>
+          {this.renderPlayers()}
+        </ScrollView>
       </View>
     )
   }
@@ -125,7 +128,6 @@ const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
     alignItems: 'center',
-    paddingHorizontal: 12,
     height: heightPercentageToDP(15)
   }
 })
